7fa42d838a65f24f7451373e55576d3fd238d843
yogaprograman <101821276+yogaprograman@users.noreply.github.com>
2022-03-24 23:10:55 +0700

    menambah file pemrograman program.cs dan taxi.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace membuatTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object dari class Taxi
+            Taxi taxi = new Taxi();
+
+
+            //pengesetan nilai properties
+            bool OnDuty = true;
+            int NumPassenger = 10;
+            taxi.DriverName = "Yoga Prayudha";
+            taxi.OnDuty = "Yes"; Convert.ToString(OnDuty);
+            taxi.NumPassenger = Convert.ToString(NumPassenger);
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace membuatTaxi
+{
+    public class Taxi
+
+    {   //properties
+        public string DriverName { get; set; }
+        public string OnDuty { get; set; }
+        public string NumPassenger { get; set; }
+
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty? : {0}", OnDuty);
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+
+        }
+    }
+}
